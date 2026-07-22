540bad222235fbeff532303eecd2c76824428180
zym <zyuz19990106@gmail.com>
2026-07-22 18:33:42 +0800

    Stabilize Android 16 Java bridge gating

--- bindings/gumjs/gumffi.h
+++ bindings/gumjs/gumffi.h
@@ -14,11 +14,10 @@
 
 G_BEGIN_DECLS
 
-typedef union _GumFFIValue GumFFIValue;
+typedef union _GumFFIArg GumFFIArg;
+typedef union _GumFFIArg GumFFIValue;
 
-#if G_BYTE_ORDER == G_LITTLE_ENDIAN
-
-union _GumFFIValue
+union _GumFFIArg
 {
   gpointer v_pointer;
   gint v_sint;
@@ -38,6 +37,10 @@ union _GumFFIValue
   gint64 v_sint64;
   guint64 v_uint64;
 };
+
+#if G_BYTE_ORDER == G_LITTLE_ENDIAN
+
+typedef union _GumFFIArg GumFFIRet;
 
 #else
 
@@ -70,7 +73,9 @@ union _GumFFIValue
 
 # pragma pack (push, 1)
 
-union _GumFFIValue
+typedef union _GumFFIRet GumFFIRet;
+
+union _GumFFIRet
 {
 # if GLIB_SIZEOF_VOID_P == 8
   /* Unpadded 64-bit types */
@@ -167,6 +172,8 @@ union _GumFFIValue
 extern ffi_type gum_ffi_type_size_t;
 extern ffi_type gum_ffi_type_ssize_t;
 
+G_GNUC_INTERNAL void gum_ffi_arg_to_ret (const ffi_type * type, GumFFIArg * arg,
+    GumFFIRet * ret);
 G_GNUC_INTERNAL gboolean gum_ffi_try_get_type_by_name (const gchar * name,
     ffi_type ** type);
 G_GNUC_INTERNAL gboolean gum_ffi_try_get_abi_by_name (const gchar * name,
--- bindings/gumjs/runtime/java.js
+++ bindings/gumjs/runtime/java.js
@@ -1,1 +1,77 @@
-Frida._java = require('frida-java-bridge');
+if (shouldLoadJavaBridge()) {
+  Frida._java = require('frida-java-bridge');
+} else {
+  Frida._java = makeAndroid16JavaBridgeStub();
+}
+
+function shouldLoadJavaBridge() {
+  if (Process.platform !== 'linux')
+    return true;
+
+  const apiLevel = getAndroidApiLevel();
+  if (apiLevel === null || apiLevel < 36)
+    return true;
+
+  return hasJavaBridgeOverrideFile();
+}
+
+function getAndroidApiLevel() {
+  try {
+    const systemPropertyGet = new NativeFunction(
+      Module.getExportByName('libc.so', '__system_property_get'),
+      'int', ['pointer', 'pointer']);
+    const key = Memory.allocUtf8String('ro.build.version.sdk');
+    const value = Memory.alloc(92);
+    const length = systemPropertyGet(key, value);
+    if (length <= 0)
+      return null;
+    return parseInt(value.readUtf8String(length), 10);
+  } catch (e) {
+    return null;
+  }
+}
+
+function hasJavaBridgeOverrideFile() {
+  const paths = [
+    '/data/local/tmp/frida-enable-java-bridge',
+    '/data/local/tmp/.frida-enable-java-bridge'
+  ];
+
+  for (const path of paths) {
+    try {
+      File.readAllText(path);
+      return true;
+    } catch (e) {
+    }
+  }
+
+  return false;
+}
+
+function makeAndroid16JavaBridgeStub() {
+  const unavailable = () => {
+    throw new Error('Android API 36 Java bridge is gated by this stability build; create /data/local/tmp/frida-enable-java-bridge to opt in');
+  };
+
+  return Object.freeze({
+    available: false,
+    perform: unavailable,
+    performNow: unavailable,
+    scheduleOnMainThread: unavailable,
+    enumerateLoadedClasses: unavailable,
+    enumerateLoadedClassesSync: unavailable,
+    enumerateClassLoaders: unavailable,
+    enumerateClassLoadersSync: unavailable,
+    use: unavailable,
+    openClassFile: unavailable,
+    choose: unavailable,
+    retain: unavailable,
+    cast: unavailable,
+    array: unavailable,
+    registerClass: unavailable,
+    deoptimizeEverything: unavailable,
+    deoptimizeBootImage: unavailable,
+    deoptimizeMethod: unavailable,
+    backtrace: unavailable
+  });
+}
